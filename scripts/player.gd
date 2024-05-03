5e757af86285eb2dd5cbd8b8734f0960a5b1bbeb
bellicapelli0 <orlandoscarpa95@gmail.com>
2024-05-03 11:20:53 +0200

    player movement rework/cleanup

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,15 +4,55 @@ extends CharacterBody2D
 @export var sprite: AnimatedSprite2D
 @export var interaction_area: Area2D
 
-var speed := 8000.0
-var jump_velocity := -300.0
+@export_category("stats")
+@export var speed := 3000.
+@export var acceleration := 800.
+@export var friction := 1600.
+@export var air_friction := 1000.
+@export var jump_velocity := -300.
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = Global.GRAVITY
 
-signal first_jump
+signal first_jump # emitted when first jumping, enables double jump
 
-func a_press():
+func _ready():
+	sprite.play()
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor(): # Gravity
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("A"):
+		a_press()
+	elif Input.is_action_just_pressed("B"):
+		b_press()
+
+	var direction : float = Input.get_axis("Left", "Right")
+
+	if direction:
+		sprite.scale.x = int(direction) # flip sprite based on direction
+		accelerate(direction, delta)
+		sprite.animation = "walk"
+	else:
+		add_friction(delta)
+		sprite.animation = "idle"
+
+	move_and_slide()
+
+func accelerate(direction: float, delta: float) -> void:
+	velocity.x = move_toward(velocity.x, speed * direction, acceleration * delta)
+
+func add_friction(delta: float) -> void:
+	if is_on_floor(): # floor friction
+		velocity.x = move_toward(velocity.x, 0., friction * delta)
+	else: # air friction
+		velocity.x = move_toward(velocity.x, 0., air_friction * delta)
+		
+func jump() -> void:
+	velocity.y = jump_velocity
+
+func a_press() -> void:
 	var collisions := interaction_area.get_overlapping_areas()
 	if len(collisions) > 0:
 		for area in collisions:
@@ -22,37 +62,5 @@ func a_press():
 		first_jump.emit()
 		jump()
 
-func _ready():
-	sprite.play()
-	
-
-func _physics_process(delta: float):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("A"):
-		a_press()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("Left", "Right")
-
-	# set direction of player
-	if direction:
-		if direction > 0:
-			sprite.scale.x = 1
-		elif direction < 0:
-			sprite.scale.x = -1
-
-		velocity.x = direction * speed * delta
-		sprite.animation = "walk"
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		sprite.animation = "idle"
-
-	move_and_slide()
-	
-func jump():
-	velocity.y = jump_velocity
+func b_press() -> void:
+	pass

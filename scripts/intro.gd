d77d9ec6e9a3df4cfef05c5f8cdc696a24f10117
Hedd Schotte <subzerofusion@gmail.com>
2024-05-31 01:45:45 +1000

    changed how intro worked

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -8,19 +8,20 @@ extends Node2D
 var panel := 0
 
 func next():
-	if panel > len(panels): return
+	if panel >= len(panels): return
 
 	if !animation.is_playing():
 		panel += 1
-		if panel >= len(panels):
-			Global.instance.go_to_next_phase()
-		else:
+		if panel < len(panels):
 			animation.play("dither_out")
 			await animation.animation_finished
 			for i in len(panels):
 				panels[i].visible = panel == i
 			animation.play("dither_in")
 			await animation.animation_finished
+		else:
+			Global.instance.go_to_next_phase()
+
 	else:
 		animation.advance(1)
 
